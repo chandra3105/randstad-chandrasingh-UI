03d9eb2a71c115757b1717f5417aa85807773729
chandra <chandrasingh.c2006@gmail.com>
2020-10-23 01:25:20 +0530

    chandra singh ui task

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,54 @@
 function tabButClick(whoClick) {
-  if (whoClick == "abcd") {
-    document.getElementById("myCheck").checked = true;
-    document.getElementById("myCheck").disabled = true;
-  }
-  if (whoClick == "xyz") {
-    document.getElementById("myCheck1").checked = true;
-    document.getElementById("myCheck1").disabled = true;
-  }
-  if (whoClick == "three") {
-    document.getElementById("myCheck3").checked = true;
-    document.getElementById("myCheck3").disabled = true;
-  }
-  if (whoClick == "four") {
-    document.getElementById("myCheck4").checked = true;
-    document.getElementById("myCheck4").disabled = true;
-  }
-  if (whoClick == "five") {
-    document.getElementById("myCheck5").checked = true;
-    document.getElementById("myCheck5").disabled = true;
-  }
-  if (whoClick == "six") {
-    document.getElementById("myCheck6").checked = true;
-    document.getElementById("myCheck6").disabled = true;
-  }
-  if (whoClick == "seven") {
-    document.getElementById("myCheck7").checked = true;
-    document.getElementById("myCheck7").disabled = true;
-  }
+  if (whoClick == "getStartes") {
+    document.getElementById("getStartes").checked = true;
+    document.getElementById("getStartes").disabled = true;
+    document.getElementById("activeIcon").classList.add("mystyle");}
+  if (whoClick == "property") {
+    document.getElementById("property").checked = true;
+    document.getElementById("property").disabled = true;
+    document.getElementById("propertyActiveIcon").classList.add("mystyle");}
+  if (whoClick == "income") {
+    document.getElementById("income").checked = true;
+    document.getElementById("income").disabled = true;
+    document.getElementById("incomeActiveIcon").classList.add("mystyle");}
+  if (whoClick == "assets") {
+    document.getElementById("assets").checked = true;
+    document.getElementById("assets").disabled = true;
+    document.getElementById("assetsActiveIcon").classList.add("mystyle");}
+  if (whoClick == "realEstate") {
+    document.getElementById("realEstate").checked = true;
+    document.getElementById("realEstate").disabled = true;
+    document.getElementById("realEstateActiveIcon").classList.add("mystyle");}
+  if (whoClick == "declaration") {
+    document.getElementById("declaration").checked = true;
+    document.getElementById("declaration").disabled = true;
+    document.getElementById("declarationActiveIcon").classList.add("mystyle");}
+  if (whoClick == "EConsent") {
+    document.getElementById("EConsent").checked = true;
+    document.getElementById("EConsent").disabled = true;
+    document.getElementById("EConsentActiveIcon").classList.add("mystyle");}
   var checkboxes = document.querySelectorAll('input[type=checkbox]:checked').length;
   document.getElementById("child").style.transition = "all 0.6s";
-  if (checkboxes == 1) { document.getElementById("child").style.width = "15%"; }
-  if (checkboxes == 2) { document.getElementById("child").style.width = "30%"; }
-  if (checkboxes == 3) { document.getElementById("child").style.width = "45%"; }
-  if (checkboxes == 4) { document.getElementById("child").style.width = "60%"; }
-  if (checkboxes == 5) { document.getElementById("child").style.width = "75%"; }
-  if (checkboxes == 6) { document.getElementById("child").style.width = "90%"; }
-  if (checkboxes == 7) { document.getElementById("child").style.width = "100%"; }
+  if (checkboxes == 1) {
+    document.getElementById("child").style.width = "15%";
+    document.getElementById("tooltipValue").innerHTML = "15 %";}
+  if (checkboxes == 2) {
+    document.getElementById("child").style.width = "30%";
+    document.getElementById("tooltipValue").innerHTML = "30 %";}
+  if (checkboxes == 3) {
+    document.getElementById("child").style.width = "45%";
+    document.getElementById("tooltipValue").innerHTML = "45 %";}
+  if (checkboxes == 4) {
+    document.getElementById("child").style.width = "60%";
+    document.getElementById("tooltipValue").innerHTML = "60 %";}
+  if (checkboxes == 5) {
+    document.getElementById("child").style.width = "75%";
+    document.getElementById("tooltipValue").innerHTML = "75 %";}
+  if (checkboxes == 6) {
+    document.getElementById("child").style.width = "90%";
+    document.getElementById("tooltipValue").innerHTML = "90 %";}
+  if (checkboxes == 7) {
+    document.getElementById("child").style.width = "100%";
+    document.getElementById("tooltipValue").innerHTML = "100 %";
+    document.getElementById('submit').disabled = false;}
 }
